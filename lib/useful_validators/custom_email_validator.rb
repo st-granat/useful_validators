973cf0736ef613d30fe2de5d9010f5cb0a1528d5
Andrey <railscode@gmail.com>
2013-07-13 15:50:56 +0700

    phone methods

--- lib/useful_validators/custom_email_validator.rb
+++ lib/useful_validators/custom_email_validator.rb
@@ -1,18 +1,20 @@
 # coding: utf-8
 module CustomEmailValidator
-  def self.email_regex
-    /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
-  end
+  class << self
+    def email_regex
+      /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
+    end
 
-  def self.email_name_regex
-    '[\w\.%\+\-]+'.freeze
-  end
+    def email_name_regex
+      '[\w\.%\+\-]+'.freeze
+    end
 
-  def self.domain_head_regex
-    '(?:[A-Z0-9\-]+\.)+'.freeze
-  end
+    def domain_head_regex
+      '(?:[A-Z0-9\-]+\.)+'.freeze
+    end
 
-  def self.domain_tld_regex
-    '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
+    def domain_tld_regex
+      '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
+    end
   end
 end
